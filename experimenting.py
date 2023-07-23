b136d3f183d859d2e88feabbf4ae0651d036efba
LukeJenningsMQ <61956662+LukeJenningsMQ@users.noreply.github.com>
2023-07-23 18:38:15 +1000

    Update experimenting.py

--- experimenting.py
+++ experimenting.py
@@ -1,29 +1,20 @@
 
 from transformers import pipeline
-from transformers import RobertaTokenizer, RobertaModel, AutoModelForSequenceClassification, AutoTokenizer
+from transformers import RobertaTokenizer, RobertaModel, AutoModelForSequenceClassification, AutoTokenizer, logging
 
-tokenizerRoberta = AutoTokenizer.from_pretrained('roberta-base')
-modelRoberta = AutoModelForSequenceClassification.from_pretrained('roberta-base')
+
+logging.set_verbosity_warning()
+tokenizerRoberta = AutoTokenizer.from_pretrained('bert-base-cased')
+modelRoberta = AutoModelForSequenceClassification.from_pretrained('bert-base-cased')
 modelGPT = "distilgpt2"
 
-def sentimentAnalysis(text, modelName):
-    classifier = pipeline("sentiment-analysis", model=modelName, tokenizer=tokenizerRoberta)
+def sentimentAnalysis(text):
+    classifier = pipeline("sentiment-analysis")
     predictions = classifier(text)
-
-    # Get the predicted class.
-    predicted_class = predictions["label"]
-
-    # Print the predicted class.
-    print(f"The predicted class is: {predicted_class}.")
-
-    # Get the confidence score.
-    score = predictions["score"]
-
-    # Print the confidence score.
-    print(f"The confidence score is: {score}.")
+    print(predictions)
     
-def zeroShotClassification(text, labels, modelName):
-    classifier = pipeline("zero-shot-classification", model=modelName, tokenizer=tokenizerRoberta)
+def zeroShotClassification(text, labels):
+    classifier = pipeline("zero-shot-classification")
     res = classifier(text, candidate_labels=labels)
     print(res)
     
@@ -35,6 +26,6 @@ def textGeneration(text, modelName):
     
 textInput = "i love trees. We can grow them and make the world a better place as i provides oxygen for earth. Flower seeds grass ocean"
 labelsInput = ["Cyber Security", "Engineering", "Art", "Science", "Computing", "Law", "Business","Sport", "Nature"]
-sentimentAnalysis(textInput, modelRoberta)
-zeroShotClassification(textInput, labelsInput, modelRoberta)
+sentimentAnalysis(textInput)
+zeroShotClassification(textInput, labelsInput)
 textGeneration(textInput, modelGPT)
