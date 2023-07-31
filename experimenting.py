0ec305a2c44693294ef9fc0b806d6da1392dc62e
LukeJenningsMQ <61956662+LukeJenningsMQ@users.noreply.github.com>
2023-07-31 10:25:56 +1000

    fixing embedding issue

--- experimenting.py
+++ experimenting.py
@@ -2,12 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 from transformers import pipeline
-from transformers import RobertaTokenizer, BertModel, AutoModelForSequenceClassification, AutoTokenizer, logging,AutoModelForTokenClassification,BertForTokenClassification, TokenClassificationPipeline
-
-logging.set_verbosity_warning()
-tokenizerBERT = AutoTokenizer.from_pretrained("QCRI/bert-base-multilingual-cased-pos-english")
-modelBERT = BertModel.from_pretrained('QCRI/bert-base-multilingual-cased-pos-english', output_hidden_states = True)
-modelGPT = "distilgpt2"
+from transformers import RobertaTokenizer, BertModel, BertTokenizer, AutoModelForSequenceClassification, AutoTokenizer, logging,AutoModelForTokenClassification,BertForTokenClassification, TokenClassificationPipeline
 
 def sentimentAnalysis(text):
     classifier = pipeline("sentiment-analysis")
@@ -25,7 +20,7 @@ def textGeneration(text, modelName):
     print(res)
     
 def stringToTokens(text, tokeniser):
-    input = tokeniser(text)
+    input = tokeniser.tokenize(text)
     print(input)
     return input
 
@@ -41,7 +36,7 @@ def PartOfSpeech(text,modelUsed,tokeniserUsed):
     print(outputs)
     return outputs
 
-def similarWords(text, modelUsed, tokeniserUsed):
+def similarWords(text, modelUsed, tokeniserUsed,word1Index,word2Index):
     tokens = stringToTokens(text,tokeniserUsed)
     idsForTokens = tokeniserUsed.convert_tokens_to_ids(tokens)
     token_tensor = torch.tensor([idsForTokens])
@@ -50,6 +45,15 @@ def similarWords(text, modelUsed, tokeniserUsed):
     with torch.no_grad():
         output = modelUsed(token_tensor)
         print(output)
+        for i in range(1, 13):
+           last_hidden_state = output[2][i]
+           print(last_hidden_state)
+           
+          # word_embed_1 = last_hidden_state
+           #e = word_embed_1[0][word1Index].reshape(1,-1)
+           #s = word_embed_1[0][word2Index].reshape(1,-1)
+           #similarity = cosine_similarity(s,e)
+           #similarities.append(similarity[0][0])
     return similarities
 
 def plotSimilarities(similarities):
@@ -61,14 +65,21 @@ def plotSimilarities(similarities):
     plt.xlabel("Layer")
     plt.ylabel("Cosine Similarity")
 
-textInput = "Engineers need to follow a proper schedule, and to do this, they should use tools such as a Gantt Chart or a critical path method."
-labelsInput = ["Cyber Security", "Engineering", "Art", "Science", "Computing", "Law", "Business","Sport", "Nature"]
-sentimentAnalysis(textInput)
-zeroShotClassification(textInput, labelsInput)
-textGeneration(textInput, modelGPT)
-tokens = stringToTokens(textInput, tokenizerBERT)
-PartOfSpeech(textInput,modelBERT,tokenizerBERT)
-similar = similarWords(textInput,modelBERT,tokenizerBERT)
+
+
+#tokenizerBERT = BertTokenizer.from_pretrained("QCRI/bert-base-multilingual-cased-pos-english")
+#modelBERT = BertModel.from_pretrained('QCRI/bert-base-multilingual-cased-pos-english', output_hidden_states = True)
+tokenizerBERT = BertTokenizer.from_pretrained("bert-base-uncased")
+modelBERT = BertModel.from_pretrained('bert-base-uncased', output_hidden_states = True)
+#modelGPT = "distilgpt2"
+textInput = "Engineers need to follow a proper schedule and to do this, they should use tools such as a Gantt Chart or a critical path method"
+#labelsInput = ["Cyber Security", "Engineering", "Art", "Science", "Computing", "Law", "Business","Sport", "Nature"]
+#sentimentAnalysis(textInput)
+#zeroShotClassification(textInput, labelsInput)
+#textGeneration(textInput, modelGPT)
+#tokens = stringToTokens(textInput, tokenizerBERT)
+#PartOfSpeech(textInput,modelBERT,tokenizerBERT)
+similar = similarWords(textInput,modelBERT,tokenizerBERT,0,6)
 #plotSimilarities(similar)
 
 
